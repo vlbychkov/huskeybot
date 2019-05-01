a5ffa30f71d82af2ef2676c92a6e2ffb04764cdb
Huzzky <49246934+Huzzky@users.noreply.github.com>
2019-05-01 16:20:06 +0300

    Update Other.py

--- Other.py
+++ Other.py
@@ -1,5 +1,7 @@
 maneTimeDate = datetime.datetime.today().strftime("%d.%m.%Y")
 maneDate=datetime.datetime.today().strftime("%m")
+maneYear=datetime.datetime.today().strftime("%Y")
+maneyear=datetime.datetime.today().strftime("%y")
 index = 0
 massivDate = []
 wordUp = set('qwertyuiopasdfghjklzxcvbnmаоуыэяеёюибвгдйжзклмнпрстфхцчшщQWERTYUIOPASDFGHJKLZXCVBNMАОУЫЭЯЕЁЮИБВГДЙЖЗКЛМНПРСТФХЦЧШЩ')
