be393eeda0294a3db2ee605f19ad878cd3d7017e
Huzzky <49246934+Huzzky@users.noreply.github.com>
2019-05-01 16:17:55 +0300

    Add files via upload

---
+++ Lib.py
@@ -0,0 +1,9 @@
+import telebot
+import datetime
+import time # time.ctime()
+from telebot import types
+from telebot.types import Message
+import random
+import os
+import random
+import sqlite3
---
+++ Other.py
@@ -0,0 +1,5 @@
+maneTimeDate = datetime.datetime.today().strftime("%d.%m.%Y")
+maneDate=datetime.datetime.today().strftime("%m")
+index = 0
+massivDate = []
+wordUp = set('qwertyuiopasdfghjklzxcvbnmаоуыэяеёюибвгдйжзклмнпрстфхцчшщQWERTYUIOPASDFGHJKLZXCVBNMАОУЫЭЯЕЁЮИБВГДЙЖЗКЛМНПРСТФХЦЧШЩ')
