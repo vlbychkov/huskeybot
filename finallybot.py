b788098866df0adbfd941d004877465bc2e8dd45
Huzzky <49246934+Huzzky@users.noreply.github.com>
2019-05-03 23:09:08 +0300

    Update finallybot.py

--- finallybot.py
+++ finallybot.py
@@ -1,7 +1,7 @@
 from Lib import *
 from Other import *
 #нужные библиотеки
-TOKEN = '815806413:AAGh41rYw3mok9SvLPePMGjBrjAKX7zyvag'
+TOKEN = '815806413:AAF9qIka3JzDNOkH3oRYDPCcfSAqRX5_tJM'
 bot = telebot.TeleBot(TOKEN)
 menu_remove=types.ReplyKeyboardRemove() 
 
